3f062aeb6455eff4c0dcb8fd2c73ddad87fd50d0
Brian Larsen <balarsen@gmail.com>
2012-08-19 20:02:39 -0600

    first cut, not good of Trick

--- Trick.py
+++ Trick.py
@@ -1,39 +1,39 @@
 
 
 
-class Trick(dict):
+class Trick(list):
     def __init__(self, leader='north', trump=None):
-        super(Trick, self).__init__(self)
         self.leader=leader
-        self.sides = ['north', 'east', 'south', 'west', 'north', 'east', 'south', 'west']
         self.order = None
-        self.nCards = 0
         self.trump = trump
 
-    def setLeader(self, card):
-        self[self.leader] = card
-        self.order = self.sides[self.sides.index('south'):self.sides.index('south')+4]
-        self.nCards += 1
-
     def nextCard(self, card):
-        self[self.order[self.nCards]] = card
-        self.nCards += 1
+        self.append(card)
 
     def winner(self):
-        if self.nCards != 4:
+        if len(self) != 4:
             raise(ValueError('Must have 4 cards in the trick'))
-        tmp = [self['north'], self['south'], self['west'], self['east']]
+        tmp = self
         tmp.sort()
         winning_card = tmp[-1]
 
-        for key, val in self.iteritems():
-            if val == winning_card:
-                print winning_card
+        print winning_card
 
 
 
 
-
-
+if __name__ == "__main__":
+    import Card
+    t = Trick(trump='hearts')
+    print t
+    t.nextCard(Card.Card(3,3))
+    print t
+    t.nextCard(Card.Card(5,2, True))
+    print t
+    t.nextCard(Card.Card(7,3))
+    print t
+    t.nextCard(Card.Card(9,3))
+    print t
+    t.winner()
 
 
