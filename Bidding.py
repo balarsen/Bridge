4154c76540fc398baaf33d8f8359a9acefaaa375
Brian Larsen <balarsen@gmail.com>
2012-08-21 09:23:10 -0400

    Bidding mechanics are done with testing.

--- Bidding.py
+++ Bidding.py
@@ -1,8 +1,6 @@
 
 import numpy as np
 
-from __init__ import suits
-import Bid
 
 """
 this module is quite complicated, it will need to both do the mechanics of bidding
@@ -17,18 +15,42 @@ class Bidding(list):
         _seats = ['north', 'east', 'south', 'west']
         leader_ind = _seats.index(self.leader)
         self._seats = np.roll(['north', 'east', 'south', 'west'], leader_ind).tolist()
+        self._passCount = 0
 
     def addBid(self, seat, bid):
+        if self._passCount >= 4:  # done
+            raise(ValueError('Four passes have already occured'))
         if len(self) > 0: # new bid mist be larger than the old one
-            if not bid > self[-1][1]:
-                raise(ValueError('New bid must be higher than old bid'))
+            try:
+                old_winner = self._winningBid()[1]
+            except TypeError:
+                old_winner = None
+            if old_winner is not None: # we have another bid
+                if not bid > old_winner:
+                    raise(ValueError('New bid must be higher than old bid'))
+        if bid.value == 'pass':
+            self._passCount += 1
+        else:
+            self._passCount = 0
         self.append( (seat, bid) )
+        if self._passCount >= 4:  # done
+            return self._winningBid()
+        else:
+            return None
+
+    def _winningBid(self):
+        """
+        figure the winning bid
+        """
+        for seat, bid in reversed(self):
+            if bid.value != 'pass':
+                return (seat, bid)
 
     def nextBid(self, bid):
         """
         subset of above, just goes in order
         """
-        self.addBid(self._seats[len(self)%4], bid)
+        return self.addBid(self._seats[len(self)%4], bid)
 
 
 
