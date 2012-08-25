7697ab3db5fb17ec1cf5fffb3cddfdcd84105ec4
Brian Larsen <balarsen@gmail.com>
2012-08-25 14:06:07 -0400

    test and coverage updates

--- test_bridge.py
+++ test_bridge.py
@@ -5,6 +5,9 @@ from test_card import *
 from test_hand import *
 from test_deck import *
 from test_trick import *
+from test_bid import *
+from test_bidding import *
+
 
 if __name__ == '__main__':
     unittest.main()
