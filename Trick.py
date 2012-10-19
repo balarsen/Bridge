293792a1b85d7bb4dd3c710bd7b6609f4927f641
Brian Larsen <balarsen@lanl.gov>
2012-10-18 19:55:39 -0600

    filled in a finished Trick and its tests

--- Trick.py
+++ Trick.py
@@ -1,39 +1,37 @@
 
+from Table import index2pos, _positions
 
 
 class Trick(list):
-    def __init__(self, leader='north', trump=None):
-        self.leader=leader
-        self.order = None
-        self.trump = trump
+    def __init__(self, leader='North', trump=None):
+        if leader not in _positions:
+            raise(ValueError('bad leader position: {0}'.format(leader)))
+        self.leader = leader
+        self.order  = None
+        self.trump  = trump
 
     def nextCard(self, card):
+        if self.nCards >= 4:
+            raise(IndexError('Cannot have more than 4 cards in a trick'))
+        if card in self:
+            raise(ValueError('Cannot play the same card twice in a trick')) 
+        card.trump = card.suit==self.trump
         self.append(card)
 
     def winner(self):
         if len(self) != 4:
-            raise(ValueError('Must have 4 cards in the trick'))
-        tmp = self
-        tmp.sort()
-        winning_card = tmp[-1]
+            raise(IndexError('Must have 4 cards in the trick'))
+        # there are just 4 cards, look at the first then compare to the next
+        #    the highest is the winner
+        win = self[0]
+        for c in self[1:]:
+            if c > win:
+                win = c
+        # win is the winning card, where was it?
+        ind = self.index(win)
+        return win, index2pos(self.leader, ind)
 
-        print winning_card
-
-
-
-
-if __name__ == "__main__":
-    import Card
-    t = Trick(trump='hearts')
-    print t
-    t.nextCard(Card.Card(3,3))
-    print t
-    t.nextCard(Card.Card(5,2, True))
-    print t
-    t.nextCard(Card.Card(7,3))
-    print t
-    t.nextCard(Card.Card(9,3))
-    print t
-    t.winner()
-
+    @property
+    def nCards(self):
+        return len(self)
 
---
+++ test_trick.py
@@ -0,0 +1,48 @@
+
+import unittest
+
+import Card
+import Trick
+
+class TestTrick(unittest.TestCase):
+    def test_init(self):
+        """init does some checking"""
+        self.assertRaises(ValueError, Trick.Trick, leader='badval')
+        tck = Trick.Trick()
+        self.assertEqual('North', tck.leader)
+        self.assertTrue(tck.trump is None)
+        tck = Trick.Trick(leader='South', trump='diamonds')
+        self.assertEqual('South', tck.leader)
+        self.assertEqual('diamonds', tck.trump)
+
+    def test_nextCard(self):
+        """should be able to add cards"""
+        t = Trick.Trick(trump='hearts')
+        t.nextCard(Card.Card(3,3))
+        self.assertEqual(t.nCards, len(t))
+        self.assertEqual(t.nCards, 1)
+        t.nextCard(Card.Card(5,2))
+        self.assertRaises(ValueError, t.nextCard, Card.Card(5,2))
+        t.nextCard(Card.Card(7,3))
+        t.nextCard(Card.Card(9,3))
+        self.assertEqual(t.nCards, 4)
+        self.assertRaises(IndexError, t.nextCard, Card.Card(10,3))
+        self.assertFalse(t[0].trump)
+        self.assertTrue( t[1].trump)
+        self.assertFalse(t[2].trump)
+        self.assertFalse(t[3].trump)
+
+    def test_winner(self):
+        """winner should be named correctly"""
+        t = Trick.Trick(trump='hearts')
+        t.nextCard(Card.Card(3,3))
+        self.assertRaises(IndexError, t.winner)
+        t.nextCard(Card.Card(5,2))
+        t.nextCard(Card.Card(7,3))
+        t.nextCard(Card.Card(9,3))
+        self.assertEqual(t.winner(), (Card.Card(5,2), 'East'))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
