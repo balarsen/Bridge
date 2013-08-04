e5af8432b069892af4ae51c95e61209a2805cdda
Brian Larsen <balarsen@gmail.com>
2013-08-04 14:30:02 -0800

    BeginningBidder.py: return a Bid object

--- BeginningBidder.py
+++ BeginningBidder.py
@@ -6,19 +6,20 @@ the player will not open with less than 15
  - the player opens in the strongest suit
 """
 
+import Bid
 import Bidding
 
 class BeginningBidder(Bidding.Bidding_logic):
     def openBid(self):
         if self.hand.hc <= 14:
-            return ('pass', 'spades')
+            return Bid.Bid('pass', 1)
         else:
             if len(getattr(self.hand, self.hand.strongest)) > 3:
-                return (1, self.hand.strongest)
+                return Bid.Bid(1, self.hand.strongest)
             elif len(getattr(self.hand, self.hand.longest)) > 3:
-                return (1, self.hand.longest)
+                return Bid.Bid(1, self.hand.longest)
             else:
-                return (1, self.hand.strongest)
+                return Bid.Bid(1, self.hand.strongest)
 
     def raiseBid(self):
         pass
