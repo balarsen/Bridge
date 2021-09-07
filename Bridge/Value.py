94ff3dcb189dde2144c9cc7f899f2abb62ddde4c
Brian Larsen <balarsen@lanl.gov>
2021-09-07 14:50:15 -0600

    Bridge/Value.py: working value and tests

---
+++ Bridge/Value.py
@@ -0,0 +1,30 @@
+class Value(object):
+    pass
+
+
+class Value(object):
+    values = (2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A')
+    values_set = set(values)
+
+    def __init__(self, value):
+        if value not in Value.values_set:
+            raise (ValueError(f'Bad value {Value.values}'))
+        self.value = value
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def __ne__(self, other):
+        return self.value != other.value
+
+    def __gt__(self, other):
+        return self.values.index(self.value) > self.values.index(other.value)
+
+    def __lt__(self, other):
+        return self.values.index(self.value) < self.values.index(other.value)
+
+    def __str__(self):
+        return f"{self.value}"
+
+    def __repr__(self):
+        return f"<{self.value}>"
