cb907fc52200b81a94f5d0b1c880614d99df1738
Brian Larsen <balarsen@lanl.gov>
2021-09-07 14:55:53 -0600

    tests: minor cleanup

--- Bridge/tests/test_Suit.py
+++ Bridge/tests/test_Suit.py
@@ -2,22 +2,27 @@ import pytest
 
 from ..Suit import Suit
 
+
 def test_ValueError():
     with pytest.raises(ValueError):
         Suit('bad')
+
 
 def test_eq():
     a = Suit('spades')
     b = Suit('spades')
     assert a == b
 
+
 def test_ne():
     a = Suit('spades')
     b = Suit('hearts')
     assert a != b
 
+
 def test_str():
     assert str(Suit('spades')) == 'spades'
+
 
 def test_repr():
     assert repr(Suit('spades')) == '<spades>'
--- Bridge/tests/test_Value.py
+++ Bridge/tests/test_Value.py
@@ -2,32 +2,39 @@ import pytest
 
 from ..Value import Value
 
+
 def test_ValueError():
     with pytest.raises(ValueError):
         Value('bad')
+
 
 def test_eq():
     a = Value(2)
     b = Value(2)
     assert a == b
 
+
 def test_ne():
     a = Value(2)
     b = Value(3)
     assert a != b
+
 
 def test_gt():
     a = Value(2)
     b = Value(3)
     assert b > a
 
+
 def test_lt():
     a = Value(2)
     b = Value(3)
     assert a < b
 
+
 def test_str():
     assert str(Value('Q')) == 'Q'
+
 
 def test_repr():
     assert repr(Value('Q')) == '<Q>'
